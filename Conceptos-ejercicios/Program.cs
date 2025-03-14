63e9b15d6e6506942053bf31b4fb958cabbe16c3
Leimar <leimarxxx7@gmail.com>
2025-03-14 13:45:35 -0500

    commit Par-e-Impar

---
+++ Conceptos-ejercicios/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un número, por favor");
+int numero;
+if (int.TryParse(Console.ReadLine(), out numero))
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine($"El Numero es par.");
+    }
+    else
+    {
+        Console.WriteLine($"El Numero  es impar.");
+    }
+}
+else
+{
+    Console.WriteLine("Entrada no valida");
+}
+
